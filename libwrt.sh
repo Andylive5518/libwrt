c2f20eb0c5c9b76cbaedcf94c1a7d81f5cf80564
Alex <39101473@qq.com>
2025-06-28 14:17:05 +0800

    refactor(依赖管理): 优化软件包管理逻辑
    - 将kenzok8/openwrt-packages重命名为custom1并移动特定包到kenzo目录
    - 清理small包中不需要的科学上网插件
    - 移除重复的包管理操作，简化流程

--- libwrt.sh
+++ libwrt.sh
@@ -87,10 +87,12 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest package/lu
 # rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/luci/applications/{*passwall,*homeproxy,*openclash,*mosdns,*smartdns}
 rm -rf feeds/packages/net/{adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/kenzo
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/custom1
+mv package/custom1/{*adguardhome,*ddns*,*argon-*,*-argon,*design*,*lucky,*smartdns,*store,luci-lib*}  package/kenzo
+rm -rf package/custom1
 # 科学上网插件
 git clone --depth=1 https://github.com/kenzok8/small package/small
+rm -rf package/small/{*ssr-plus,*openclash,*bypass}
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
