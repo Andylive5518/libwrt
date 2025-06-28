4bb20f94c5d5fd7e928399bbb25d9c9d7f7a3f85
Alex <39101473@qq.com>
2025-06-28 14:38:00 +0800

    modified:   libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -88,7 +88,8 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-netspeedtest package/lu
 rm -rf feeds/luci/applications/{*passwall,*homeproxy,*openclash,*mosdns,*smartdns}
 rm -rf feeds/packages/net/{adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 git clone --depth=1 https://github.com/kenzok8/openwrt-packages package/custom1
-mv package/custom1/{*adguardhome,*ddns*,*argon-*,*-argon,*design*,*lucky,*smartdns,*store,luci-lib*}  package/kenzo
+mkdir -p package/kenzo
+mv package/custom1/{*adguardhome,*ddns*,*argon-*,*-argon,*design*,*lucky,*smartdns,*store,luci-lib*}  package/kenzo/
 rm -rf package/custom1
 # 科学上网插件
 git clone --depth=1 https://github.com/kenzok8/small package/small
